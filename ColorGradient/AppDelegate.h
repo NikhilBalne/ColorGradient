b24d0cc3a77b22b03685af758e8a37a25a42e18b
Nikhil Balne <nikhilkumar.balne@gmail.com>
2019-06-06 10:09:56 +0530

    Initial Commit

---
+++ ColorGradient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColorGradient
+//
+//  Created by iHub on 06/06/19.
+//  Copyright © 2019 iHubTechnologiesPvtLtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ColorGradient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColorGradient
+//
+//  Created by iHub on 06/06/19.
+//  Copyright © 2019 iHubTechnologiesPvtLtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
